d18709721eb0f8fdfb899a59281a6b7d09d7acb8
Tatsuhiko Motoki <s19c1115em@s.chibakkoudai.jp>
2024-01-20 18:44:25 +0900

    Add file and test

---
+++ maxtest.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2023 Tatsuhiko Motoki
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng(){
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+### I/O TEST ###
+out=$(seq 5 | ./max)
+[ "${out}" = 5 ] || ng ${LINENO}
+
+out=$(seq -f "%0.2f" 1 0.1 2 | ./max)
+[ "${out}" = 2.00 ] || ng ${LINENO}
+
+
+
+[ "$res" = 0 ] && echo OK
+exit $res
+
